53e4f8a192421ab4c0f6a5cc3149deff3dbb96a8
DW Rodrigues <denys.w.r@hotmail.com>
2022-03-30 21:48:12 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Imagens.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly Imagens.Data.AppContext _appContext;
+
+        public ProdutoController(Imagens.Data.AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        // GET: ProdutoController
+        public async Task<IActionResult> Index()
+        {
+            var allProducts = await _appContext.Produtos.ToListAsync();
+            return View(allProducts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
